36b6871c8273a700ae4c64b7ead8ae233fc0436f
tedsuo <ted@radicaldesigns.org>
2014-04-12 17:48:41 -0700

    version 0.2.0 * moved Router creation to a standard NewRouter funciton * added Routes#PathForHandler convenience method * GoDoc style comments for all

--- router.go
+++ router.go
@@ -4,65 +4,19 @@ import (
 	"fmt"
 	"github.com/bmizerany/pat"
 	"net/http"
-	"net/url"
 	"strings"
 )
 
+// Handlers map Handler keys to hhtp.Handler objects.  The Handler key is used to
+// then match the Handler to the appropriate Route in the Router.
 type Handlers map[string]http.Handler
 
-type Params map[string]string
-
-type Route struct {
-	Handler string
-	Method  string
-	Path    string
-}
-
-func (r Route) PathWithParams(params Params) (string, error) {
-	components := strings.Split(r.Path, "/")
-	for i, c := range components {
-		if len(c) == 0 {
-			continue
-		}
-		if c[0] == ':' {
-			val, ok := params[c[1:]]
-			if !ok {
-				return "", fmt.Errorf("missing param %s", c)
-			}
-			components[i] = val
-		}
-	}
-
-	u, err := url.Parse(strings.Join(components, "/"))
-	if err != nil {
-		return "", err
-	}
-	return u.String(), nil
-}
-
-type Routes []Route
-
-func (r Routes) RouteForHandler(handler string) (Route, bool) {
-	for _, route := range r {
-		if route.Handler == handler {
-			return route, true
-		}
-	}
-	return Route{}, false
-}
-
-func (r Routes) PathForHandler(handler string, params Params) (string, error) {
-	route, ok := r.RouteForHandler(handler)
-	if !ok {
-		return "", fmt.Errorf("No route exists for handler %", handler)
-	}
-	return route.PathWithParams(params)
-}
-
-func (r Routes) Router(actions Handlers) (http.Handler, error) {
+// NewRouter combines a set of Routes with their corresponding Handlers to
+// produce a http request multiplexer (AKA a "router").
+func NewRouter(routes Routes, handlers Handlers) (http.Handler, error) {
 	p := pat.New()
-	for _, route := range r {
-		handler, ok := actions[route.Handler]
+	for _, route := range routes {
+		handler, ok := handlers[route.Handler]
 		if !ok {
 			return nil, fmt.Errorf("missing handler %s", route.Handler)
 		}
--- router_test.go
+++ router_test.go
@@ -10,15 +10,10 @@ import (
 )
 
 var _ = Describe("Router", func() {
-	var (
-		r    http.Handler
-		resp *httptest.ResponseRecorder
-		err  error
-	)
-
 	Describe("Route", func() {
+		var route router.Route
+
 		Describe("PathWithParams", func() {
-			var route router.Route
 			BeforeEach(func() {
 				route = router.Route{
 					Handler: "whatevz",
@@ -74,99 +69,106 @@ var _ = Describe("Router", func() {
 		})
 	})
 
-	Describe("Routes#RouteForHandler", func() {
+	Describe("Routes", func() {
 		var routes router.Routes
-		BeforeEach(func() {
-			routes = router.Routes{
-				{Path: "/something", Method: "GET", Handler: "getter"},
-				{Path: "/something", Method: "POST", Handler: "poster"},
-				{Path: "/something", Method: "PuT", Handler: "putter"},
-				{Path: "/something", Method: "DELETE", Handler: "deleter"},
-			}
-		})
 
-		Context("when the route is present", func() {
-			It("returns the route with the matching handler name", func() {
-				route, ok := routes.RouteForHandler("getter")
-				Ω(ok).Should(BeTrue())
-				Ω(route.Method).Should(Equal("GET"))
-			})
-		})
-
-		Context("when the route is not present", func() {
-			It("returns falseness", func() {
-				route, ok := routes.RouteForHandler("orangutanger")
-				Ω(ok).Should(BeFalse())
-				Ω(route).Should(BeZero())
-			})
-		})
-	})
-
-	Describe("Routes#PathForHandler", func() {
-		var routes router.Routes
-		BeforeEach(func() {
-			routes = router.Routes{
-				{
-					Handler: "whatevz",
-					Method:  "GET",
-					Path:    "/a/path/:param/with/:many_things/:many/in/:it",
-				},
-			}
-		})
-
-		Context("when the route is present", func() {
-			It("returns the route with the matching handler name", func() {
-				path, err := routes.PathForHandler("whatevz", router.Params{
-					"param":       "1",
-					"many_things": "2",
-					"many":        "a space",
-					"it":          "4",
-				})
-				Expect(err).NotTo(HaveOccurred())
-				Ω(path).Should(Equal(`/a/path/1/with/2/a%20space/in/4`))
-			})
-
-			Context("when the route is not present", func() {
-				It("returns an error", func() {
-					_, err := routes.PathForHandler("foo", router.Params{
-						"param":       "1",
-						"many_things": "2",
-						"many":        "a space",
-						"it":          "4",
-					})
-					Expect(err).To(HaveOccurred())
-				})
-			})
-
-			Context("when the hash is missing params", func() {
-				It("should error", func() {
-					_, err := routes.PathForHandler("whatevz", router.Params{
-						"param": "1",
-						"many":  "2",
-						"it":    "4",
-					})
-					Ω(err).Should(HaveOccurred())
-				})
-			})
-		})
-	})
-
-	Describe("Routes#Router", func() {
-		Context("when all the handlers are present", func() {
+		Describe("RouteForHandler", func() {
 			BeforeEach(func() {
-				resp = httptest.NewRecorder()
-				routes := router.Routes{
+				routes = router.Routes{
 					{Path: "/something", Method: "GET", Handler: "getter"},
 					{Path: "/something", Method: "POST", Handler: "poster"},
 					{Path: "/something", Method: "PuT", Handler: "putter"},
 					{Path: "/something", Method: "DELETE", Handler: "deleter"},
 				}
-				r, err = routes.Router(router.Handlers{
-					"getter":  test_server.Respond(http.StatusOK, "get response"),
-					"poster":  test_server.Respond(http.StatusOK, "post response"),
-					"putter":  test_server.Respond(http.StatusOK, "put response"),
-					"deleter": test_server.Respond(http.StatusOK, "delete response"),
+			})
+
+			Context("when the route is present", func() {
+				It("returns the route with the matching handler name", func() {
+					route, ok := routes.RouteForHandler("getter")
+					Ω(ok).Should(BeTrue())
+					Ω(route.Method).Should(Equal("GET"))
 				})
+			})
+
+			Context("when the route is not present", func() {
+				It("returns falseness", func() {
+					route, ok := routes.RouteForHandler("orangutanger")
+					Ω(ok).Should(BeFalse())
+					Ω(route).Should(BeZero())
+				})
+			})
+		})
+
+		Describe("PathForHandler", func() {
+			BeforeEach(func() {
+				routes = router.Routes{
+					{
+						Handler: "whatevz",
+						Method:  "GET",
+						Path:    "/a/path/:param/with/:many_things/:many/in/:it",
+					},
+				}
+			})
+
+			Context("when the route is present", func() {
+				It("returns the route with the matching handler name", func() {
+					path, err := routes.PathForHandler("whatevz", router.Params{
+						"param":       "1",
+						"many_things": "2",
+						"many":        "a space",
+						"it":          "4",
+					})
+					Expect(err).NotTo(HaveOccurred())
+					Ω(path).Should(Equal(`/a/path/1/with/2/a%20space/in/4`))
+				})
+
+				Context("when the route is not present", func() {
+					It("returns an error", func() {
+						_, err := routes.PathForHandler("foo", router.Params{
+							"param":       "1",
+							"many_things": "2",
+							"many":        "a space",
+							"it":          "4",
+						})
+						Expect(err).To(HaveOccurred())
+					})
+				})
+
+				Context("when the hash is missing params", func() {
+					It("should error", func() {
+						_, err := routes.PathForHandler("whatevz", router.Params{
+							"param": "1",
+							"many":  "2",
+							"it":    "4",
+						})
+						Ω(err).Should(HaveOccurred())
+					})
+				})
+			})
+		})
+	})
+
+	Describe("Router", func() {
+		var r http.Handler
+		var err error
+		var routes = router.Routes{
+			{Path: "/something", Method: "GET", Handler: "getter"},
+			{Path: "/something", Method: "POST", Handler: "poster"},
+			{Path: "/something", Method: "PuT", Handler: "putter"},
+			{Path: "/something", Method: "DELETE", Handler: "deleter"},
+		}
+
+		Context("when all the handlers are present", func() {
+			var resp *httptest.ResponseRecorder
+			var handlers = router.Handlers{
+				"getter":  test_server.Respond(http.StatusOK, "get response"),
+				"poster":  test_server.Respond(http.StatusOK, "post response"),
+				"putter":  test_server.Respond(http.StatusOK, "put response"),
+				"deleter": test_server.Respond(http.StatusOK, "delete response"),
+			}
+			BeforeEach(func() {
+				resp = httptest.NewRecorder()
+				r, err = router.NewRouter(routes, handlers)
 				Ω(err).ShouldNot(HaveOccurred())
 			})
 
@@ -200,27 +202,26 @@ var _ = Describe("Router", func() {
 		})
 
 		Context("when a handler is missing", func() {
+			var incompleteHandlers = router.Handlers{
+				"getter": test_server.Respond(http.StatusOK, "get response"),
+			}
 			It("should error", func() {
-				routes := router.Routes{
-					{Path: "/something", Method: "GET", Handler: "getter"},
-					{Path: "/something", Method: "POST", Handler: "poster"},
-				}
-				r, err = routes.Router(router.Handlers{
-					"getter": test_server.Respond(http.StatusOK, "get response"),
-				})
+				r, err = router.NewRouter(routes, incompleteHandlers)
 
 				Ω(err).Should(HaveOccurred())
 			})
 		})
 
-		Context("with an invalid verb", func() {
+		Context("with an invalid method", func() {
+			var invalidRoutes = router.Routes{
+				{Path: "/something", Method: "SMELL", Handler: "smeller"},
+			}
+
 			It("should error", func() {
-				routes := router.Routes{
-					{Path: "/something", Method: "SMELL", Handler: "smeller"},
-				}
-				r, err = routes.Router(router.Handlers{
+				handlers := router.Handlers{
 					"smeller": test_server.Respond(http.StatusOK, "smelt response"),
-				})
+				}
+				r, err = router.NewRouter(invalidRoutes, handlers)
 
 				Ω(err).Should(HaveOccurred())
 			})
