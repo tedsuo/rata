f37f1815b8642351268bdfaf59033c448311c3df
tedsuo <ted@tedsuo.com>
2016-09-16 14:53:30 -0700

    Support all valid HTTP Methods
    * added CONNECT, HEAD, OPTIONS, PATCH, and TRACE
    * as a side effect, GET routes no longer automatically register HEAD routes

--- router_test.go
+++ router_test.go
@@ -158,16 +158,27 @@ var _ = Describe("Router", func() {
 			{Path: "/something", Method: "POST", Name: "poster"},
 			{Path: "/something", Method: "PuT", Name: "putter"},
 			{Path: "/something", Method: "DELETE", Name: "deleter"},
+			{Path: "/something", Method: "OPTIONS", Name: "optioner"},
+			{Path: "/something", Method: "patch", Name: "patcher"},
+			{Path: "/something", Method: "HEAD", Name: "header"},
+			{Path: "/something", Method: "CONNEcT", Name: "connector"},
+			{Path: "/something", Method: "TRACE", Name: "tracer"},
 		}
 
 		Context("when all the handlers are present", func() {
 			var resp *httptest.ResponseRecorder
 			var handlers = rata.Handlers{
-				"getter":  ghttp.RespondWith(http.StatusOK, "get response"),
-				"poster":  ghttp.RespondWith(http.StatusOK, "post response"),
-				"putter":  ghttp.RespondWith(http.StatusOK, "put response"),
-				"deleter": ghttp.RespondWith(http.StatusOK, "delete response"),
+				"getter":    ghttp.RespondWith(http.StatusOK, "get response"),
+				"poster":    ghttp.RespondWith(http.StatusOK, "post response"),
+				"putter":    ghttp.RespondWith(http.StatusOK, "put response"),
+				"deleter":   ghttp.RespondWith(http.StatusOK, "delete response"),
+				"optioner":  ghttp.RespondWith(http.StatusOK, "options response"),
+				"patcher":   ghttp.RespondWith(http.StatusOK, "patch response"),
+				"header":    ghttp.RespondWith(http.StatusOK, "head response"),
+				"connector": ghttp.RespondWith(http.StatusOK, "connect response"),
+				"tracer":    ghttp.RespondWith(http.StatusOK, "trace response"),
 			}
+
 			BeforeEach(func() {
 				resp = httptest.NewRecorder()
 				r, err = rata.NewRouter(routes, handlers)
@@ -204,6 +215,46 @@ var _ = Describe("Router", func() {
 
 				r.ServeHTTP(resp, req)
 				Ω(resp.Body.String()).Should(Equal("delete response"))
+			})
+
+			It("makes OPTIONS handlers", func() {
+				req, err := http.NewRequest("OPTIONS", "/something", nil)
+				Ω(err).ShouldNot(HaveOccurred())
+
+				r.ServeHTTP(resp, req)
+				Ω(resp.Body.String()).Should(Equal("options response"))
+			})
+
+			It("makes PATCH handlers", func() {
+				req, err := http.NewRequest("PATCH", "/something", nil)
+				Ω(err).ShouldNot(HaveOccurred())
+
+				r.ServeHTTP(resp, req)
+				Ω(resp.Body.String()).Should(Equal("patch response"))
+			})
+
+			It("makes HEAD handlers", func() {
+				req, err := http.NewRequest("HEAD", "/something", nil)
+				Ω(err).ShouldNot(HaveOccurred())
+
+				r.ServeHTTP(resp, req)
+				Ω(resp.Body.String()).Should(Equal("head response"))
+			})
+
+			It("makes CONNECT handlers", func() {
+				req, err := http.NewRequest("CONNECT", "/something", nil)
+				Ω(err).ShouldNot(HaveOccurred())
+
+				r.ServeHTTP(resp, req)
+				Ω(resp.Body.String()).Should(Equal("connect response"))
+			})
+
+			It("makes TRACE handlers", func() {
+				req, err := http.NewRequest("TRACE", "/something", nil)
+				Ω(err).ShouldNot(HaveOccurred())
+
+				r.ServeHTTP(resp, req)
+				Ω(resp.Body.String()).Should(Equal("trace response"))
 			})
 		})
 
